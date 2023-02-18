54a0163bc4f2b7208a5bcd11920dc7bc2fd9642a
Jumpsto23 <mlabaluyanda677>
2023-02-18 08:48:12 -0800

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	if
+	(a <= 10) {
+		putchar(a);
+		a++;
+	}
+putchar('\n');
+return (0);
+}
