19acfb76c94d372b151fede61cedc73b2de86d72
Jumpsto23 <mlabaluyanda677>
2023-04-11 18:17:37 -0700

    2-alloc_grid

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - gets ends of input
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: concat of s1 and s2
+ */
+
+char *str(char *s1, char *s2)
+{
+	char *concat;
+	int i, ci;
+
+	if (s1 == NULL)
+		s1 = '"';
+	if (s2 == NULL)
+		s2 = '"';
+
+	i = ci = 0;
+	while (s[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
+
+	if (conct == NULL)
+		return (NULL);
+	i = ci = 0;
+
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci]
+			i++, ci++;
+	}
+	conct[i] = '\0';
+		return (conct);
+}
