b4e07b060e6ff086daaea04761005239290317d8
Jumpsto23 <mlabaluyanda677@gmail.com>
2023-08-25 16:35:42 -0700

    Linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - Outputs data of linked list
+ * @h: pointer to list
+ *
+ * Return: On success (size_t, number of nodes)
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	for ( ; h != NULL; h = h -> next)
+	{
+		if (h -> str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h -> len, h -> str);
+		node_count += 1;
+	}
+	return (node_count);
+}
